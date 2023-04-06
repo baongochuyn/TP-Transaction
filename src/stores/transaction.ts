3e9434b7a051b2b8580d63a121f49b867897921c
BaoNgoc <baongochuynh.hbn@gmail.com>
2023-04-06 22:44:57 +0200

    add transactionStore

--- src/stores/transaction.ts
+++ src/stores/transaction.ts
@@ -4,4 +4,30 @@ import type { Transaction } from '../entities/transaction'
 
 export const useTransactionStore = defineStore('transaction', function () {
   const transaction = ref<Transaction[]>([])
+  const newId = Number(
+    transaction.value.length === 0 ? 1 : Math.max(...transaction.value.map((t) => t.id)) + 1
+  )
+  const addTransaction = (itemAdd: any) => {
+    transaction.value.push({
+      id: newId,
+      category: '',
+      description: itemAdd.description,
+      date: new Date(itemAdd.date),
+      amount: BigInt(itemAdd.amount)
+    })
+  }
+
+  const total = computed(() => {
+    const value = transaction.value.reduce(
+      (acc, transaction) => acc + transaction.amount,
+      BigInt(0)
+    )
+    return value
+  })
+
+  return {
+    addTransaction,
+    transaction,
+    total
+  }
 })
