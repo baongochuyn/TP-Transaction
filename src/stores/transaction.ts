45d83acde4ea0d9f4cfe7a85c31e7ce1293b7b60
BaoNgoc <baongochuynh.hbn@gmail.com>
2023-04-11 23:05:12 +0200

    Show total by categogy

--- src/stores/transaction.ts
+++ src/stores/transaction.ts
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import type { Transaction } from '../entities/transaction'
+import { Category } from '@/entities/category'
 
 export const useTransactionStore = defineStore('transaction', function () {
   const transaction = ref<Transaction[]>([])
@@ -15,12 +16,17 @@ export const useTransactionStore = defineStore('transaction', function () {
     })
   }
 
+  const totalByCategory = (category: string) => {
+    let sum = BigInt(0)
+
+    for (let i = 0; i < transaction.value.length; i++) {
+      if (transaction.value[i].category == category) {
+        sum += BigInt(transaction.value[i].amount)
+      }
+    }
+    return sum
+  }
   const total = computed(() => {
-    // const value = transaction.value.reduce(
-    //   (acc, transaction) => acc + transaction.amount,
-    //   BigInt(0)
-    // )
-    // return value
     let total = BigInt(0) // Khởi tạo giá trị tổng ban đầu là 0 với kiểu BigInt
 
     for (let i = 0; i < transaction.value.length; i++) {
@@ -32,6 +38,7 @@ export const useTransactionStore = defineStore('transaction', function () {
   return {
     addTransaction,
     transaction,
-    total
+    total,
+    totalByCategory
   }
 })
--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -2,6 +2,11 @@
   <h1>Liste des transactions</h1>
   <TableTransaction />
   <p>Solde du compte : {{ total }}</p>
+  <ul>
+    <li v-for="(item, index) in categoryStore.category" :key="index">
+      {{ item.name }} : {{ transactionStore.totalByCategory(item.name) }} €
+    </li>
+  </ul>
   <RouterLink to="/new-transaction"><button>Ajouter une transaction</button></RouterLink>
   <RouterLink to="/new-category"><button>Ajouter une catégory</button></RouterLink>
 </template>
@@ -9,8 +14,9 @@
 <script setup lang="ts">
 import TableTransaction from '@/components/TableTransaction.vue'
 import { useTransactionStore } from '@/stores/transaction'
+import { useCategoryStore } from '@/stores/category'
 
 const transactionStore = useTransactionStore()
-
+const categoryStore = useCategoryStore()
 const total = transactionStore.total
 </script>
